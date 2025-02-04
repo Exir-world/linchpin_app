7812867b1206f91a42d442a62d71c35083f77345
farhadrezvani7 <farhadrezvani7@gmail.com>
2025-02-04 11:46:54 +0330

    feat: Fix login problem

--- lib/features/time_management/data/data_source/api_time_mamagement.dart
+++ lib/features/time_management/data/data_source/api_time_mamagement.dart
@@ -10,6 +10,7 @@ class ApiTimeMamagement {
   Future<dynamic> daily(String actionType) async {
     final response = await httpclient.post('attendance/main-page', data: {
       "actionType": actionType,
+      "workReport": "string",
     });
     return response;
   }
